4119dcbe8c7a2f21ef3c2fd2b0c86fa461e0d9ef
Victor Poutot <v.poutot@gmail.com>
2021-06-15 09:16:00 +0200

    première fenêtre modal

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # OCproject6
+
+Pour installer l'API local visitez https://github.com/istaion/OCproject6
--- app.js
+++ app.js
@@ -21,6 +21,31 @@ fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score")
         document
           .querySelector(".best-movie__second-block > p")
           .innerText = value.description;
+        document
+          .querySelector("#best-movie-modal h1")
+          .innerText = value.title;
+        document
+          .querySelector("#best-movie-modal h1")
+          .insertAdjacentHTML('afterend', '<img src=' + value.image_url + ' alt="best-movie">'
+                              + '<p> genre :' + value.genres + '</p>'
+                              + '<p> date de sortie :' + value.date_published + '</p>'
+                              + '<p> rated :' + value.rated + '</p>'
+                              + '<p> score Imdb :' + value.imdb_score + '</p>'
+                              + '<p> réalisateur :' + value.directors + '</p>'
+                              + '<p> acteurs :</p>'
+                            );
+        value.actors.forEach(actor => {
+          document
+            .querySelector("#best-movie-modal ul")
+            .insertAdjacentHTML('beforeend', '<li>' + actor + '</li>');
+        });
+        document
+          .querySelector("#best-movie-modal ul")
+          .insertAdjacentHTML('afterend', '<p> durée :' + value.duration + 'min </p>'
+                              + '<p> pays d’origine :' + value.countries + '</p>'
+                              + '<p> résultat au Box Office :' + value.votes + '</p>'
+                              + '<p> résumé :' + value.long_description + '</p>'
+                            );
       })
       .catch(function(err) {
         // Une erreur est survenue
@@ -29,3 +54,18 @@ fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score")
   .catch(function(err) {
     // Une erreur est survenue
   });
+
+var modal = document.getElementById("best-movie-modal");
+var btn = document.getElementsByClassName("best-movie__button")[0];
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
